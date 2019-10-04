05eb511bc642c077ea985093ef819e9bffb1add2
Ezzat Chamudi <chamudi.git@outlook.com>
2019-10-04 14:49:05 +0300

    Add initial files

---
+++ README.md
@@ -0,0 +1,77 @@
+# Kanji
+
+Get kanji characters in an array based on categorization.
+
+## Usage
+
+```js
+const { kanken, jlpt } = require('kanji');
+
+// Kanji Kentei
+console.log(kanken.lv10);
+console.log(kanken.lv09);
+console.log(kanken.lv08);
+console.log(kanken.lv07);
+console.log(kanken.lv06);
+console.log(kanken.lv05);
+console.log(kanken.lv04);
+console.log(kanken.lv03);
+console.log(kanken.lv02pre);
+console.log(kanken.lv02);
+console.log(kanken.lv01pre);
+console.log(kanken.lv01);
+console.log(kanken.extra);
+
+// Old JLPT
+console.log(jlpt.old4);
+console.log(jlpt.old3);
+console.log(jlpt.old2);
+console.log(jlpt.old1);
+
+// New JLPT
+console.log(jlpt.n5);
+console.log(jlpt.n4);
+console.log(jlpt.n3);
+console.log(jlpt.n2);
+console.log(jlpt.n1);
+```
+
+
+The kanji characters in the frequency list array are ordered from most popular to least popular.
+The rest of arrays are not ordered.
+
+## Development
+
+| Branch | Status |
+| - | - |
+| master | [![Build Status](https://travis-ci.org/ezhmd/kanji.svg?branch=master)](https://travis-ci.org/ezhmd/kanji) |
+| develop | [![Build Status](https://travis-ci.org/ezhmd/kanji.svg?branch=develop)](https://travis-ci.org/ezhmd/kanji) |
+
+### Testing
+```
+npm test
+```
+## References
+
+The items are collected from following sources.
+
+- JLPT Study by Peter van der Woude https://jlptstudy.net
+- Jonathan Waller JLPT Kanji List http://www.tanos.co.uk/jlpt/
+- KANJIDIC Project http://www.edrdg.org/wiki/index.php/KANJIDIC_Project
+- Kanshudo Kanji Collections https://www.kanshudo.com/collections
+- nihongo-pro.com https://nihongo-pro.com/
+- 日本漢字能力検定級別漢字表 https://www.kanken.or.jp/kanken/outline/degree.html
+- 漢字辞典 https://kanjijoho.com
+- 辞典オンライン https://jitenon.jp
+
+## Authors
+
+* **Ezzat Chamudi** - [ezhmd](https://github.com/ezhmd)
+
+See also the list of [contributors](https://github.com/ezhmd/kanji/graphs/contributors) who participated in this project.
+
+## License
+
+Kanji code is licensed under [MPL-2.0](https://www.mozilla.org/en-US/MPL/2.0/). Images, logos, docs, and articles in this Kanji project are released under [CC-BY-4.0](https://creativecommons.org/licenses/by/4.0/legalcode).
+
+Libraries, dependencies, and tools used in this project are tied with their own licenses respectively.
---
+++ test/test.js
@@ -0,0 +1,153 @@
+'strict mode';
+
+const assert = require('assert');
+const kanji = require('../');
+
+const allArrays = [
+    kanji.kanken.lv10, // 0
+    kanji.kanken.lv09, // 1
+    kanji.kanken.lv08, // 2
+    kanji.kanken.lv07, // 3
+    kanji.kanken.lv06, // 4
+    kanji.kanken.lv05, // 5
+    kanji.kanken.lv04, // 6
+    kanji.kanken.lv03, // 7
+    kanji.kanken.lv02pre, // 8
+    kanji.kanken.lv02, // 9
+    kanji.kanken.lv01pre, // 10
+    kanji.kanken.lv01, // 11
+    kanji.jlpt.old4, // 12
+    kanji.jlpt.old3, // 13
+    kanji.jlpt.old2, // 14
+    kanji.jlpt.old1, // 15
+    kanji.jlpt.n5, // 16
+    kanji.jlpt.n4, // 17
+    kanji.jlpt.n3, // 18
+    kanji.jlpt.n2, // 19
+    kanji.jlpt.n1, // 20
+    kanji.grade.g01, // 21
+    kanji.grade.g02, // 21
+    kanji.grade.g03, // 23
+    kanji.grade.g04, // 24
+    kanji.grade.g05, // 25
+    kanji.grade.g06, // 26
+    kanji.grade.g08, // 27
+    kanji.grade.g09, // 28
+    kanji.grade.g10, // 29
+    kanji.freq, // 30
+    kanji.all // 31
+];
+
+describe('testing Kanji', function () {
+    describe('testing flat file kanji list', function() {
+        it('has kanji kentei properties', function() {
+            assert.deepStrictEqual(Array.isArray(kanji.kanken.lv10), true);
+            assert.deepStrictEqual(Array.isArray(kanji.kanken.lv09), true);
+            assert.deepStrictEqual(Array.isArray(kanji.kanken.lv08), true);
+            assert.deepStrictEqual(Array.isArray(kanji.kanken.lv07), true);
+            assert.deepStrictEqual(Array.isArray(kanji.kanken.lv06), true);
+            assert.deepStrictEqual(Array.isArray(kanji.kanken.lv05), true);
+            assert.deepStrictEqual(Array.isArray(kanji.kanken.lv04), true);
+            assert.deepStrictEqual(Array.isArray(kanji.kanken.lv03), true);
+            assert.deepStrictEqual(Array.isArray(kanji.kanken.lv02pre), true);
+            assert.deepStrictEqual(Array.isArray(kanji.kanken.lv02), true);
+            assert.deepStrictEqual(Array.isArray(kanji.kanken.lv01pre), true);
+            assert.deepStrictEqual(Array.isArray(kanji.kanken.lv01), true);
+        });
+
+        it('has jlpt properties', function() {
+            assert.deepStrictEqual(Array.isArray(kanji.jlpt.old4), true);
+            assert.deepStrictEqual(Array.isArray(kanji.jlpt.old3), true);
+            assert.deepStrictEqual(Array.isArray(kanji.jlpt.old2), true);
+            assert.deepStrictEqual(Array.isArray(kanji.jlpt.old1), true);
+
+            assert.deepStrictEqual(Array.isArray(kanji.jlpt.n5), true);
+            assert.deepStrictEqual(Array.isArray(kanji.jlpt.n4), true);
+            assert.deepStrictEqual(Array.isArray(kanji.jlpt.n3), true);
+            assert.deepStrictEqual(Array.isArray(kanji.jlpt.n2), true);
+            assert.deepStrictEqual(Array.isArray(kanji.jlpt.n1), true);
+        });
+
+        it('has grade properties', function() {
+            // Kyouiku
+            assert.deepStrictEqual(Array.isArray(kanji.grade.g01), true);
+            assert.deepStrictEqual(Array.isArray(kanji.grade.g02), true);
+            assert.deepStrictEqual(Array.isArray(kanji.grade.g03), true);
+            assert.deepStrictEqual(Array.isArray(kanji.grade.g04), true);
+            assert.deepStrictEqual(Array.isArray(kanji.grade.g05), true);
+            assert.deepStrictEqual(Array.isArray(kanji.grade.g06), true);
+
+            // Remaining Joyo
+            assert.deepStrictEqual(Array.isArray(kanji.grade.g08), true);
+
+            // Jinmeiyo
+            assert.deepStrictEqual(Array.isArray(kanji.grade.g09), true);
+
+            // Variant of joyo kanji
+            assert.deepStrictEqual(Array.isArray(kanji.grade.g10), true);
+        });
+
+        it('has freq properties', function() {
+            assert.deepStrictEqual(Array.isArray(kanji.freq), true);
+        });
+
+        it('has all properties', function() {
+            // 13,108 kanji from KANJIDIC (JIS X 0208-1998, JIS X 0212-1990, JIS X 0213-2012)
+            assert.deepStrictEqual(Array.isArray(kanji.all), true);
+        });
+    });
+
+    describe('uniqueness test', function() {
+        it('has unique arrays for all', function() {
+            allArrays.forEach((array, index) => {
+                const set = new Set();
+
+                array.forEach((char) => {
+                    if (!set.has(char)) set.add(char);
+                    else throw new Error('Wrong, at index ' + index + ' char ' + char);
+                })
+            })
+        });
+
+        it('has unique arrays for kanken categorization group', function() {
+            const kanken = [
+                ...kanji.kanken.lv10,
+                ...kanji.kanken.lv09,
+                ...kanji.kanken.lv08,
+                ...kanji.kanken.lv07,
+                ...kanji.kanken.lv06,
+                ...kanji.kanken.lv05,
+                ...kanji.kanken.lv04,
+                ...kanji.kanken.lv03,
+                ...kanji.kanken.lv02pre,
+                ...kanji.kanken.lv02,
+                ...kanji.kanken.lv01pre,
+                ...kanji.kanken.lv01,
+            ];
+
+            const set = new Set();
+
+            kanken.forEach((char) => {
+                if (!set.has(char)) set.add(char);
+                else throw new Error('Wrong, char ' + char);
+            })
+        });
+
+        it('has unique arrays for new jlpt categorization group', function() {
+            const jlpt = [
+                ...kanji.jlpt.n5,
+                ...kanji.jlpt.n4,
+                ...kanji.jlpt.n3,
+                ...kanji.jlpt.n2,
+                ...kanji.jlpt.n1,
+            ];
+
+            const set = new Set();
+
+            jlpt.forEach((char) => {
+                if (!set.has(char)) set.add(char);
+                else throw new Error('Wrong, char ' + char);
+            })
+        });
+    });
+});
