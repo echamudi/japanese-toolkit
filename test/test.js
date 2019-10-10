238d7b11438078e2d68add2d3abc905b813cff0e
Ezzat Chamudi <chamudi.git@outlook.com>
2019-10-10 21:23:11 +0300

    Add metadata testing

--- test/test.js
+++ test/test.js
@@ -2,7 +2,10 @@
 /* eslint-disable prefer-arrow-callback */
 
 const { execSync } = require('child_process');
+const assert = require('assert');
 const console = require('console');
+const path = require('path');
+const sqlite3 = require('sqlite3').verbose();
 
 describe('japanese-db-maker', function () {
   it('exports database', function () {
@@ -13,5 +16,29 @@ describe('japanese-db-maker', function () {
     });
 
     console.log(`Please check the result at ${__dirname}/result`);
+  });
+
+  it('extracts correctly', async function () {
+    // Random checks
+
+    const db = new sqlite3.Database(path.join(__dirname, 'result', 'japanese.db'));
+
+    const [metadataTest] = await Promise.all([
+      new Promise((resolve) => {
+        db.all('SELECT * FROM metadata', function (err, rows) {
+          resolve(rows);
+        });
+      }),
+    ]);
+
+    assert.deepStrictEqual(
+      metadataTest.find((/** @type {Object} */ el) => el.key === 'jmdict-date').value,
+      '2019-08-16',
+    );
+
+    assert.deepStrictEqual(
+      metadataTest.find((/** @type {Object} */ el) => el.key === 'jmnedict-date').value,
+      '2019-09-20',
+    );
   });
 });
