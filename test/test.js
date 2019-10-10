cfbf15281ec5315a1dc04d209a11c770282d574f
Ezzat Chamudi <chamudi.git@outlook.com>
2019-10-10 21:02:52 +0300

    Test using mocha

--- test/test.js
+++ test/test.js
@@ -1,9 +1,17 @@
+/* eslint-disable func-names */
+/* eslint-disable prefer-arrow-callback */
+
 const { execSync } = require('child_process');
 const console = require('console');
 
-console.log(`> japanese-db-maker sqlite -s ${__dirname}/fixtures -d ${__dirname}/result`);
-execSync(`japanese-db-maker sqlite -s ${__dirname}/fixtures -d ${__dirname}/result`, {
-  stdio: 'inherit',
-});
+describe('japanese-db-maker', function () {
+  it('exports database', function () {
+    console.log(`> japanese-db-maker sqlite -s ${__dirname}/fixtures -d ${__dirname}/result`);
 
-console.log(`Please check the result at ${__dirname}/result`);
+    execSync(`japanese-db-maker sqlite -s ${__dirname}/fixtures -d ${__dirname}/result`, {
+      stdio: 'inherit',
+    });
+
+    console.log(`Please check the result at ${__dirname}/result`);
+  });
+});
