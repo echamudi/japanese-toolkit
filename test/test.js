952264107fec37b9e8a857648c034616ba5aebf7
Ezzat Chamudi <chamudi.git@outlook.com>
2019-10-10 20:39:09 +0300

    Add verbose in test

--- test/test.js
+++ test/test.js
@@ -2,6 +2,8 @@ const { execSync } = require('child_process');
 const console = require('console');
 
 console.log(`> japanese-db-maker sqlite -s ${__dirname}/fixtures -d ${__dirname}/result`);
-execSync(`japanese-db-maker sqlite -s ${__dirname}/fixtures -d ${__dirname}/result`);
+execSync(`japanese-db-maker sqlite -s ${__dirname}/fixtures -d ${__dirname}/result`, {
+  stdio: 'inherit',
+});
 
 console.log(`Please check the result at ${__dirname}/result`);
