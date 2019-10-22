696fb27e287a40e0ec7d028d6c4d2427af8cb503
Ezzat Chamudi <chamudi.git@outlook.com>
2019-10-22 23:38:11 +0300

    Add wanikani audio index source

---
+++ scripts/postinstall.js
@@ -0,0 +1,9 @@
+const https = require('https');
+const fs = require('fs');
+
+fs.mkdirSync('./source/', { recursive: true });
+const wkAudioIndexJson = fs.createWriteStream('./source/wk-audio-index.json');
+
+https.get('https://raw.githubusercontent.com/ezhmd/japanese-vocabulary-pronunciation-audio/master/dist/wk-audio-index.json', (response) => {
+  response.pipe(wkAudioIndexJson);
+});
