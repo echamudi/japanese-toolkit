94e6fa9098ae54da3ff3134df2a9d7301b60373a
Ezzat Chamudi <chamudi.git@outlook.com>
2019-10-31 23:42:51 +0300

    Add error exit code

--- cli.js
+++ cli.js
@@ -15,24 +15,29 @@ program
   .option('-s, --source <source>', 'Destination folder')
   .option('-d, --destination <destination>', 'Destination folder')
   .action(async (args) => {
-    console.log('Source: ', args.source);
-    console.log('Destination: ', args.destination);
+    try {
+      console.log('Source: ', args.source);
+      console.log('Destination: ', args.destination);
 
-    if (!fs.existsSync(args.destination)) fs.mkdirSync(args.destination, { recursive: true });
+      if (!fs.existsSync(args.destination)) fs.mkdirSync(args.destination, { recursive: true });
 
-    await JapaneseDbMaker.buildSqlite(
-      // source objects
-      {
-        jmdict: path.join(args.source, 'JMdict_e'),
-        jmnedict: path.join(args.source, 'JMnedict.xml'),
-        kanjidic: path.join(args.source, 'kanjidic2.xml'),
-      },
-      // destination file
-      `${args.destination}/japanese.db`,
-    );
+      await JapaneseDbMaker.buildSqlite(
+        // source objects
+        {
+          jmdict: path.join(args.source, 'JMdict_e'),
+          jmnedict: path.join(args.source, 'JMnedict.xml'),
+          kanjidic: path.join(args.source, 'kanjidic2.xml'),
+        },
+        // destination file
+        `${args.destination}/japanese.db`,
+      );
 
-    console.log('');
-    console.log('Completed!');
+      console.log('');
+      console.log('Completed!');
+    } catch (error) {
+      console.error(error);
+      process.exit(1);
+    }
   });
 
 program.parse(process.argv);
