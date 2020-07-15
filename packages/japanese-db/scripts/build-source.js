0a44edb442d413173bd14f6759188d4a8fdde547
Ezzat Chamudi <echamudi@outlook.com>
2020-07-15 04:39:22 +0300

    Rename postinstall to build source

--- packages/japanese-db/scripts/build-source.js
+++ packages/japanese-db/scripts/build-source.js
@@ -5,5 +5,5 @@ fs.mkdirSync('./source/', { recursive: true });
 const wkAudioIndexJson = fs.createWriteStream('./source/wk-audio-index.json');
 
 https.get('https://raw.githubusercontent.com/echamudi/japanese-vocabulary-pronunciation-audio/master/dist/wk-audio-index.json', (response) => {
-  response.pipe(wkAudioIndexJson);
+    response.pipe(wkAudioIndexJson);
 });
