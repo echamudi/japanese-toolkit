88f8197ffc86484eff038bad240a91cab96c0782
Ezzat Chamudi <echamudi@outlook.com>
2020-07-15 05:29:06 +0300

    Fix styling

--- packages/japanese-db/cli.js
+++ packages/japanese-db/cli.js
@@ -5,39 +5,41 @@ const path = require('path');
 const program = require('commander');
 const console = require('console');
 const {
-  JapaneseDBTool,
+    JapaneseDBTool,
 } = require('./index');
 
 program
-  .command('sqlite')
-  .alias('toSQLite')
-  .description('Export JMdict XML to SQLite database.')
-  .option('-s, --source <source>', 'Destination folder')
-  .option('-d, --destination <destination>', 'Destination folder')
-  .action(async (args) => {
-    try {
-      console.log('Source: ', args.source);
-      console.log('Destination: ', args.destination);
+    .command('sqlite')
+    .alias('toSQLite')
+    .description('Export JMdict XML to SQLite database.')
+    .option('-s, --source <source>', 'Destination folder')
+    .option('-d, --destination <destination>', 'Destination folder')
+    .action(async (args) => {
+        try {
+            console.log('Source: ', args.source);
+            console.log('Destination: ', args.destination);
 
-      if (!fs.existsSync(args.destination)) fs.mkdirSync(args.destination, { recursive: true });
+            if (!fs.existsSync(args.destination)) {
+                fs.mkdirSync(args.destination, { recursive: true });
+            }
 
-      await JapaneseDBTool.buildSqlite(
-        // source objects
-        {
-          jmdict: path.join(args.source, 'JMdict_e'),
-          jmnedict: path.join(args.source, 'JMnedict.xml'),
-          kanjidic: path.join(args.source, 'kanjidic2.xml'),
-        },
-        // destination file
-        `${args.destination}/japanese.db`,
-      );
+            await JapaneseDBTool.buildSqlite(
+                // source objects
+                {
+                    jmdict: path.join(args.source, 'JMdict_e'),
+                    jmnedict: path.join(args.source, 'JMnedict.xml'),
+                    kanjidic: path.join(args.source, 'kanjidic2.xml'),
+                },
+                // destination file
+                `${args.destination}/japanese.db`,
+            );
 
-      console.log('');
-      console.log('Completed!');
-    } catch (error) {
-      console.error(error);
-      process.exit(1);
-    }
-  });
+            console.log('');
+            console.log('Completed!');
+        } catch (error) {
+            console.error(error);
+            process.exit(1);
+        }
+    });
 
 program.parse(process.argv);
