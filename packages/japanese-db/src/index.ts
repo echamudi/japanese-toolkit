bc07417f06fa506a938976999235f4c8c2595119
Ezzat Chamudi <echamudi@outlook.com>
2020-08-26 04:40:51 +0300

    Fix structure

---
+++ packages/japanese-db/src/index.ts
@@ -0,0 +1,25 @@
+/* Copyright (c) 2020 Ezzat Chamudi
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+import JMdictUtil from './JMdictUtil';
+import JMnedictUtil from './JMnedictUtil';
+import KanjidicUtil from './KanjidicUtil';
+import JapaneseDBTool from './JapaneseDBTool';
+
+import * as JMdict from './types/jmdict';
+import * as JMnedict from './types/jmnedict';
+import * as JapaneseDB from './types/japanesedb';
+
+export {
+    JMdictUtil,
+    JMnedictUtil,
+    KanjidicUtil,
+    JapaneseDBTool,
+    JMdict,
+    JMnedict,
+    JapaneseDB,
+};
